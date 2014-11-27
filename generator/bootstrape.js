5fc476cc380a12e9d4e1eac0e3560872366c8a9c
sandeepchhapola <schhapola0001@gmail.com>
2014-11-27 17:44:04 +0530

    color module added to differentiate logs

--- generator/bootstrape.js
+++ generator/bootstrape.js
@@ -1,18 +1,19 @@
+require('colors');
 module.exports.npmInstallation=function(){
     var spawn = require('child_process').spawn,
         npm    = spawn('npm', ['install']);
 
     npm.stdout.on('data', function (data) {
-        console.log(data.toString());
+        console.log(data.toString().blue);
     });
 
     npm.stderr.on('data', function (data) {
-        console.log('npm stderr: ' + data.toString());
+        console.log(('npm stderr: ' + data.toString()).red);
     });
 
     npm.on('close', function (code) {
         if (code !== 0) {
-            console.log('npm process exited with code ' + code.toString());
+            console.log('npm process exited with code ' + code.toString().yellow);
         }
     });
 };
@@ -22,16 +23,16 @@ module.exports.bowerInstallation=function(){
         bower    = spawn('bower', ['install']);
 
     bower.stdout.on('data', function (data) {
-        console.log(data.toString());
+        console.log(data.toString().blue);
     });
 
     bower.stderr.on('data', function (data) {
-        console.log('bower stderr: ' + data.toString());
+        console.log(('bower stderr: ' + data.toString()).red);
     });
 
     bower.on('close', function (code) {
         if (code !== 0) {
-            console.log('bower process exited with code ' + code.toString());
+            console.log(('bower process exited with code ' + code.toString()).yellow);
         }
     });
 };
