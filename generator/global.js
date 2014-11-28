60596276368f16dc6881ff13281c66f2cf56156e
sandeepchhapola <schhapola0001@gmail.com>
2014-11-28 16:55:02 +0530

    bug fixing

--- generator/global.js
+++ generator/global.js
@@ -1,7 +1,11 @@
 var fs = require('fs'),
-    path = require('path');
+    path=require('path'),
+    appBaseUrl='';
 
 module.exports.init = function () {
+    global.__defineGetter__('_appBaseUrl', function () {
+        return appBaseUrl||(appBaseUrl=process.cwd());
+    });
     global.__defineGetter__('_fs', function () {
         return fs;
     });
