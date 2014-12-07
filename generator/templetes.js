be361f98c34b146ab069cb6bde4f2ec9cfb5f809
sandeepchhapola <schhapola0001@gmail.com>
2014-12-07 20:02:05 +0530

    content file changed

--- generator/templetes.js
+++ generator/templetes.js
@@ -1,12 +1,16 @@
 module.exports.buildApp = function () {
     var filePath = _basedir + "/templete_files/content.txt";
     var newFile = '';
+    var _basedirName=_path.basename(_basedir);
     var content = _fs.readFileSync(filePath).toString().split('\n');
     var len = content.length;
     var done = 0;
     require('colors');
 
     content.forEach(function (line) {
+        if(line.indexOf('nodeguru') > -1){
+           line.replace('nodeguru',_basedirName);
+        }
         if (line.indexOf('[FILE_URL]') > -1) {
             newFile = line.replace('[FILE_URL]', _appBaseUrl);
             _fs.writeFileSync(newFile, '', "utf-8");
